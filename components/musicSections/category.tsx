5790571c263437fb41a725818e1c06cf5e178c07
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-23 14:45:13 +0100

    Remove unused vars

--- components/musicSections/category.tsx
+++ components/musicSections/category.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import SingleMusicDisplay from './singleMusicDisplay'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 const Category = ({ category }: { category: string }) => {
     return (
-        <div className='bg-primary-light dark:bg-primary-dark p-6 my-4 md:my-6 rounded-xl min-w-screen'>
+        <div className='bg-primary-light dark:bg-primary-dark p-6 my-4 md:my-6 rounded-xl'>
             <h1 className='font-semibold text-lg'>{category}</h1>
             {/* <div className='w-fit border overflow-x-auto'> */}
             <div className='flex w-full overflow-x-auto gap-y-6 md:gap-y-8 scrollbar-thin gap-4 mb-4 min-w-0'>

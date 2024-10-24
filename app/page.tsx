9f6988213eedb8637b6b98536943ce5ab01df43c
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-24 06:50:09 +0100

    working on search

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Category from "@/components/musicSections/category";
 import SearchModalWrapper from "@/components/search/searchModalWrapper";
+import { Suspense } from "react";
 
 export default function Home({ searchParams }: { searchParams?: { query?: string } }) {
   return (
     <main className="relative">
-      <SearchModalWrapper query={searchParams?.query || ""} />
+      <Suspense fallback={<div className="skeleton w-48 h-12"></div>}>
+        <SearchModalWrapper query={searchParams?.query || ""} />
+      </Suspense>
+
       <Category category="Daily Mixes" />
       <Category category="Recently" />
       <Category category="New Releases" />

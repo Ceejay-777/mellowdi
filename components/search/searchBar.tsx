4f969d1de08ae4de6e645b32295a26f418ba9a9f
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-25 11:22:05 +0100

    fix zoom screen on mobile

--- components/search/searchBar.tsx
+++ components/search/searchBar.tsx
@@ -34,7 +34,7 @@ const SearchBar = () => {
     return (
         <div className={`md:block justify-center md:w-fit w-full px-4 md:px-0 md:mr-6 ${(mobile && !isSearchPage) ? 'hidden' : 'flex'}`}>
             <label className="input input-bordered flex items-center gap-2 w-full md:my-0 my-2 rounded-full md:mr-4 h-8 md:h-12 max-w-[540px]">
-                <input type="text" className="grow text-sm md:text-base" placeholder="Search" onChange={(event) => handleSearch(event.target.value)} onFocus={() => { setSearchActive(true) }} onBlur={(event) => { if (!event.target.value) { setSearchActive(false) } }} defaultValue={searchParams.get("query")?.toString()} />
+                <input type="text" className="grow" placeholder="Search" onChange={(event) => handleSearch(event.target.value)} onFocus={() => { setSearchActive(true) }} onBlur={(event) => { if (!event.target.value) { setSearchActive(false) } }} defaultValue={searchParams.get("query")?.toString()} />
                 <Search strokeWidth={3} className='size-3 md:size-5' />
             </label>
         </div>

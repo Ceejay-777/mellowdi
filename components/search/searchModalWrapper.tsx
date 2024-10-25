c61bbe7919b3da4ba783213e41e4b40ae0a8a215
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-25 11:16:00 +0100

    fix search m

--- components/search/searchModalWrapper.tsx
+++ components/search/searchModalWrapper.tsx
@@ -4,17 +4,19 @@ import SearchResults from './searchResults'
 import { useSearchContext } from '@/context/searchContext'
 import RecentSearches from './recentSearches'
 import { useMediaQuery } from 'react-responsive'
-// import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
 const SearchModalWrapper = ({ query }: { query: string }) => {
   const { searchActive } = useSearchContext()
-  const isMobile = useMediaQuery({query: '(max-width: 768px)'})
- 
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+  const pathname = usePathname()
+  const isSearchPage = pathname === '/search'
+
 
   return (
-    (isMobile || searchActive) ? <div className='md:absolute top-20 right-8 block md:bg-zinc-200/80 md:dark:bg-zinc-900/80 p-6 md:rounded-xl'>
-      { !query && <RecentSearches />}
-      { query && <SearchResults query={query} />}
+    (isMobile || searchActive) && isSearchPage  ? <div className='md:absolute md:top-20 md:right-8 block md:bg-zinc-200/80 md:dark:bg-zinc-900/80 p-6 md:rounded-xl'>
+      {!query && <RecentSearches />}
+      {query && <SearchResults query={query} />}
     </div > : <div>
     </div>
 

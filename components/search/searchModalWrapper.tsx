8d7d91f154fbc333a7b555a0cad2ef8c61326fa8
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-10-25 11:11:38 +0100

    use mobile

--- components/search/searchModalWrapper.tsx
+++ components/search/searchModalWrapper.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 import SearchResults from './searchResults'
 import { useSearchContext } from '@/context/searchContext'
 import RecentSearches from './recentSearches'
-// import { useMediaQuery } from 'react-responsive'
+import { useMediaQuery } from 'react-responsive'
 // import { usePathname } from 'next/navigation'
 
 const SearchModalWrapper = ({ query }: { query: string }) => {
   const { searchActive } = useSearchContext()
-  // const isMobile = useMediaQuery({query: '(max-width: 768px)'})
+  const isMobile = useMediaQuery({query: '(max-width: 768px)'})
  
 
   return (
-    searchActive ? <div className='md:absolute top-20 right-8 block md:bg-zinc-200/80 md:dark:bg-zinc-900/80 p-6 md:rounded-xl'>
+    (isMobile || searchActive) ? <div className='md:absolute top-20 right-8 block md:bg-zinc-200/80 md:dark:bg-zinc-900/80 p-6 md:rounded-xl'>
       { !query && <RecentSearches />}
       { query && <SearchResults query={query} />}
     </div > : <div>
